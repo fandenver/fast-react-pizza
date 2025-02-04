546dbc84c29aa38c38eafdde5c70aa4485f3a3d8
fandenver <finanspochta22@gmail.com>
2025-02-04 19:44:27 +0300

    поправил размер поля поиска

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -17,10 +17,10 @@ function SearchOrder() {
   return (
     <form onSubmit={handleSubmit}>
       <input
-        placeholder="Search order #"
+        placeholder="Search order"
         value={query}
         onChange={e => setQuery(e.target.value)}
-        className="focus: w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm ring-opacity-50 transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 sm:w-64 sm:focus:w-72"
+        className="w-[137px] rounded-full bg-yellow-100 px-4 py-2 text-sm ring-opacity-50 transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
